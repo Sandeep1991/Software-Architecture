1406fdd86c998973d908da0790bfee8054511614
Sandeep1991 <sandeepvadiraj@yahoo.co.in>
2017-04-08 22:27:34 -0700

    updated csv

---
+++ rsf2csv.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+#{"name":"flare.analytics.cluster.AgglomerativeCluster","size":3938,"imports":["flare.animate.Transitioner","flare.vis.data.DataList","flare.util.math.IMatrix","flare.analytics.cluster.MergeEdge","flare.analytics.cluster.HierarchicalCluster","flare.vis.data.Data"]},
+
+import os
+import random as rand
+import json
+
+LINE_LIST=[]
+d1={}
+with open('apache-log4j-2.3-src_deps.rsf','r') as f1:  
+	for line in f1:
+		LINE_LIST.append(line)
+	for i in range(0,len(LINE_LIST)):
+		key=LINE_LIST[i].rstrip().split(' ')[1]
+		if key not in d1:
+			d1.setdefault(key,[])
+		value=LINE_LIST[i].rstrip().split(' ')[2]
+		d1[key].append(value)
+print len(d1)
+print key
+print d1[key]
+print len(d1[key])
+
+
+target = open('output_csv.csv', 'w')
+target.truncate()
+target.write('id,value')
+target.write("\n")
+target.write('apache,')
+target.write("\n")
+target.truncate()
+m=''
+for k in d1:
+	word_d=k.replace('.','*')
+	#word_d=wrd.replace('.','*')
+	l='apache.'+word_d+','
+	n_l='apache.'+word_d+'.'
+	target.write(l)
+	target.write("\n")
+	m=d1[k]
+	word_v=''
+	for j in m:
+		word_v=j
+		wvr=word_v.replace('.','-')
+		target.write(n_l+wvr+',100')
+		target.write("\n")
+
